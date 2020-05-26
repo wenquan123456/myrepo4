6ca0e22d77a5d7879781c2e4ce2e1a90f5158eb0
wenquan <1249593743@qq.com>
2020-05-26 23:47:52 +0800

    2020上传

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -1,10 +1,24 @@
 import org.junit.Test;
 
+import java.lang.reflect.Array;
+import java.util.*;
+
 public class Main {
 
     @Test
     public void testHelloWrold() {
-
+        List list =new ArrayList ();
+        int[]arr={1,2,3,3,1,2,};
+        Set set =new HashSet ();
+        Arrays.sort (arr);
+        for (int i = 0; i < arr.length; i++) {
+            if (!list.contains (arr)) {
+                list.add (arr[i]);
+            }
+            set.add (arr[i]);
+        }
+        System.out.println (list);
+        System.out.println (set);
         System.out.println("HelloWorld!");
     }
 }
